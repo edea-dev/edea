8dbf9827efeb680176e7521c8805e38f30d6a2fa
Zoe Boele <zoe@astech.hu>
2022-05-03 17:15:42 +0200

    added conftest.py

--- tests/test_metadata.py
+++ tests/test_metadata.py
@@ -1,5 +1,15 @@
 # here would be my tests, IF I HAD ANY
 
 class TestMetadata:
-    def test_extract(self):
-        assert True
+    def test_metadata_extraction_all_projects(self):
+        for proj_name, context in test_projects.items():
+            pro = Project(get_path_to_test_project(proj_name))
+            before = time()
+            pro.parse()
+            after = time()
+
+            metadata = pro.metadata()
+            metadata["parse_time"] = after - before
+
+            for meta_key, expected_value in context.items():
+                assert metadata[meta_key] == expected_value
