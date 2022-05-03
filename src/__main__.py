bda33b66220e4011b100727f94616a6c7d3f9035
Zoe Boele <zoe@astech.hu>
2022-05-03 16:49:41 +0200

    cleaned up argument parsing a bit

--- src/__main__.py
+++ src/__main__.py
@@ -1,12 +1,22 @@
 import argparse
+import os
+import pathlib
+from time import time
+import json
 
-from edea import schematic, from_str
+from edea import Schematic, Project
+from edea import from_str
 
-parser = argparse.argumentparser(description='parse, render and merge kicad projects')
-parser.add_argument('projects', metavar='path', type=str, nargs='+',
-                    help='kicad project folders')
-parser.add_argument('--meta', help='extract metadata and output to stdout')
-parser.add_argument('--merge', help='extract metadata and output to stdout')
+parser = argparse.ArgumentParser(description='Tool to parse, render, and merge KiCad projects.')
+pgroup = parser.add_mutually_exclusive_group()
+pgroup.add_argument('--extract-meta', help='Extract metadata from KiCad project and output to stdout or to json file.',
+                    action='store_true')
+pgroup.add_argument('--merge', action='store_true', help='Merge the listed KiCad projects into a single project ('
+                                                         'specify target directory with the output argument).')
+parser.add_argument('--output', type=str, nargs='?', default=False,
+                    help="Specify output directory for merge, or output file for metadata extraction.")
+parser.add_argument('projects', type=str, nargs='+',
+                    help='Path(s) to KiCad Project directory used as input.')
 
 args = parser.parse_args()
 
@@ -20,19 +30,21 @@ args = parser.parse_args()
 # pro.metadata()
 # pro.as_sheet()
 
-# pro.box()
+    print(json.dumps(metadata))
 
-sch1 = schematic.empty()
-sub_sch: schematic
+else:
+    # not a metadata dump operation
+    sch1 = Schematic.empty()
+    sub_sch: Schematic
 
-with open("example-module/example-module.kicad_sch", encoding="utf-8") as f:
-    sch = from_str(f.read())
-    sub_sch = schematic(sch, "example_sub", "example-module/example-module.kicad_sch")
+    with open("example-module/example-module.kicad_sch", encoding="utf-8") as f:
+        sch = from_str(f.read())
+        sub_sch = Schematic(sch, "example_sub", "example-module/example-module.kicad_sch")
 
-sch1.append(sub_sch, "sub schematic 1")
-sch1.append(sub_sch, "sub schematic 2")
+    sch1.append(sub_sch, "sub schematic 1")
+    sch1.append(sub_sch, "sub schematic 2")
 
-with open("top.kicad_sch", "w", encoding="utf-8") as f:
-    f.write(str(sch1.as_expr()))
+    with open("top.kicad_sch", "w", encoding="utf-8") as f:
+        f.write(str(sch1.as_expr()))
 
-# todo(ln): add a pin to the sub
+    # todo(ln): add a pin to the sub
